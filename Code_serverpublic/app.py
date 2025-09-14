34051c961a68d8604dfb9dbbac83f15d4b0beea6
buivanhung280702 <113452517+buivanhung280702@users.noreply.github.com>
2025-09-14 22:59:36 +0700

    Update app.py

--- Code_serverpublic/app.py
+++ Code_serverpublic/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-app.config["MONGO_URI"] = "mongodb+srv://bhung7001:Hung28072002@cluster0.s0le1.mongodb.net/greentech?retryWrites=true&w>
+app.config["MONGO_URI"] = "mongodb+srv://************@cluster0.s0le1.mongodb.net/greentech?retryWrites=true&w>
 
 mongo = PyMongo(app)
 
